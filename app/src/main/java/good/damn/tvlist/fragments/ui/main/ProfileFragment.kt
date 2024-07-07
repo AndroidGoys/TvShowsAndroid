a009815f743cc2f6feaead4a83130f577f49cafa
GoodDamn <grigorydum@gmail.com>
2024-07-07 22:04:34 +0300

    ProfileFragment: UI finally

--- app/src/main/java/good/damn/tvlist/fragments/ui/main/ProfileFragment.kt
+++ app/src/main/java/good/damn/tvlist/fragments/ui/main/ProfileFragment.kt
@@ -1,10 +1,14 @@
 package good.damn.tvlist.fragments.ui.main
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import good.damn.tvlist.App
 import good.damn.tvlist.R
 import good.damn.tvlist.extensions.boundsLinear
@@ -78,56 +82,149 @@ class ProfileFragment
         BigButtonView(
             context
         ).apply {
-
-            text = getString(
-                R.string.settings
-            )
-
-            textColor = App.color(
-                R.color.text
-            )
-
-            setCardBackgroundColor(
+            defaultBigButton(
+                this,
+                R.string.settings,
+                R.drawable.ic_gear,
+                App.drawable(
+                    R.drawable.ic_arrow_forward
+                ),
                 App.color(
-                    R.color.background
-                )
-            )
-
-            typeface = App.font(
-                R.font.open_sans_semi_bold,
-                context
-            )
-
-            imageStart = App.drawable(
-                R.drawable.ic_gear
-            )
-
-            imageEnd = App.drawable(
-                R.drawable.ic_arrow_forward
-            )
-
-            boundsLinear(
-                Gravity.CENTER_HORIZONTAL,
-                top = measureUnit * 0.072463f,
-                width = (measureUnit * 0.9227f).toInt(),
-                height = (measureUnit * 0.186f).toInt()
-            )
-
-            radius = heightParams() * 0.181818f
-            cardElevation = heightParams() * 0.07f
-
-            imageStartSizeFactor = 0.394736f
-            imageEndSizeFactor = 0.31579f
-            textSizeFactor = 0.22368f
-
-            layout.addView(
-                this
+                    R.color.text
+                ),
+                measureUnit,
+                0.072463f,
+                layout
             )
         }
+
+        BigButtonView(
+            context
+        ).apply {
+            defaultBigButton(
+                this,
+                R.string.about_app,
+                R.drawable.ic_info,
+                App.drawable(
+                    R.drawable.ic_arrow_forward
+                ),
+                App.color(
+                    R.color.text
+                ),
+                measureUnit,
+                0.036231f,
+                layout
+            )
+        }
+
+        BigButtonView(
+            context
+        ).apply {
+            defaultBigButton(
+                this,
+                R.string.rate_app,
+                R.drawable.ic_star,
+                App.drawable(
+                    R.drawable.ic_arrow_forward
+                ),
+                App.color(
+                    R.color.text
+                ),
+                measureUnit,
+                0.036231f,
+                layout
+            )
+        }
+
+        BigButtonView(
+            context
+        ).apply {
+            defaultBigButton(
+                this,
+                R.string.write_assessment,
+                R.drawable.ic_message,
+                null,
+                App.color(
+                    R.color.text
+                ),
+                measureUnit,
+                0.036231f,
+                layout
+            )
+        }
+
+        BigButtonView(
+            context
+        ).apply {
+            defaultBigButton(
+                this,
+                R.string.logout,
+                R.drawable.ic_logout_red,
+                textColor = 0xffFF7E7E.toInt(),
+                measureUnit = measureUnit,
+                topFactor = 0.036231f,
+                layout = layout
+            )
+        }
+
 
         return layout
     }
 
+}
+
+private fun ProfileFragment.defaultBigButton(
+    btn: BigButtonView,
+    @StringRes textId: Int,
+    @DrawableRes imageStartId: Int,
+    imageEnd: Drawable? = null,
+    textColor: Int,
+    measureUnit: Int,
+    topFactor: Float,
+    layout: LinearLayout
+) {
+    btn.apply {
+        text = getString(
+            textId
+        )
+
+        this.textColor = textColor
+
+        setCardBackgroundColor(
+            App.color(
+                R.color.background
+            )
+        )
+
+        typeface = App.font(
+            R.font.open_sans_semi_bold,
+            context
+        )
+
+        imageStart = App.drawable(
+            imageStartId
+        )
+
+        this.imageEnd = imageEnd
+
+        boundsLinear(
+            Gravity.CENTER_HORIZONTAL,
+            top = measureUnit * topFactor,
+            width = (measureUnit * 0.9227f).toInt(),
+            height = (measureUnit * 0.186f).toInt()
+        )
+
+        radius = heightParams() * 0.181818f
+        cardElevation = heightParams() * 0.07f
+
+        imageStartSizeFactor = 0.394736f
+        imageEndSizeFactor = 0.31579f
+        textSizeFactor = 0.22368f
+
+        layout.addView(
+            this
+        )
+    }
 }
 
 private fun ProfileFragment.onClickBtnBack(
