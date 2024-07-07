4197e62af8a61bcea00d95b10fc40ba8e76f25ae
GoodDamn <grigorydum@gmail.com>
2024-07-07 21:45:25 +0300

    ProfileFragment: UI pt1

--- app/src/main/java/good/damn/tvlist/fragments/ui/main/ProfileFragment.kt
+++ app/src/main/java/good/damn/tvlist/fragments/ui/main/ProfileFragment.kt
@@ -1,11 +1,21 @@
 package good.damn.tvlist.fragments.ui.main
 
 import android.content.Context
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import good.damn.tvlist.App
+import good.damn.tvlist.R
+import good.damn.tvlist.extensions.boundsLinear
+import good.damn.tvlist.extensions.heightParams
+import good.damn.tvlist.extensions.setBackgroundColorId
+import good.damn.tvlist.extensions.setTextColorId
+import good.damn.tvlist.extensions.setTextSizePx
 import good.damn.tvlist.fragments.StackFragment
 import good.damn.tvlist.fragments.animation.FragmentAnimation
 import good.damn.tvlist.utils.ViewUtils
+import good.damn.tvlist.views.buttons.BigButtonView
 import good.damn.tvlist.views.buttons.ButtonBack
 import java.io.Closeable
 
@@ -20,18 +30,100 @@ class ProfileFragment
             context
         )
 
-        val btnBack = ButtonBack.createLinear(
+        ButtonBack.createLinear(
             context,
             measureUnit
-        )
+        ).apply {
 
-        layout.apply {
-            addView(btnBack)
+            setOnClickListener(
+                this@ProfileFragment::onClickBtnBack
+            )
+
+            layout.addView(
+                this
+            )
         }
 
-        btnBack.setOnClickListener(
-            this::onClickBtnBack
-        )
+        TextView(
+            context
+        ).apply {
+
+            setTextSizePx(
+                measureUnit * 0.072463f
+            )
+
+            typeface = App.font(
+                R.font.open_sans_bold,
+                context
+            )
+
+            setText(
+                R.string.profile
+            )
+
+            setTextColorId(
+                R.color.text
+            )
+
+            boundsLinear(
+                left = measureUnit * 0.03865f,
+                top = measureUnit * 0.08057f
+            )
+
+            layout.addView(
+                this
+            )
+        }
+
+        BigButtonView(
+            context
+        ).apply {
+
+            text = getString(
+                R.string.settings
+            )
+
+            textColor = App.color(
+                R.color.text
+            )
+
+            setCardBackgroundColor(
+                App.color(
+                    R.color.background
+                )
+            )
+
+            typeface = App.font(
+                R.font.open_sans_semi_bold,
+                context
+            )
+
+            imageStart = App.drawable(
+                R.drawable.ic_gear
+            )
+
+            imageEnd = App.drawable(
+                R.drawable.ic_arrow_forward
+            )
+
+            boundsLinear(
+                Gravity.CENTER_HORIZONTAL,
+                top = measureUnit * 0.072463f,
+                width = (measureUnit * 0.9227f).toInt(),
+                height = (measureUnit * 0.186f).toInt()
+            )
+
+            radius = heightParams() * 0.181818f
+            cardElevation = heightParams() * 0.07f
+
+            imageStartSizeFactor = 0.394736f
+            imageEndSizeFactor = 0.31579f
+            textSizeFactor = 0.22368f
+
+            layout.addView(
+                this
+            )
+        }
 
         return layout
     }
