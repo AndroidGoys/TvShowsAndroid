1635fd1cff2c8880b85c01772e2bb023208182ed
GoodDamn <grigorydum@gmail.com>
2024-07-13 10:55:42 +0300

    TVShowPostReviewFragment: UI finally

--- app/src/main/java/good/damn/tvlist/fragments/ui/main/tv_show_details/TVShowPostReviewFragment.kt
+++ app/src/main/java/good/damn/tvlist/fragments/ui/main/tv_show_details/TVShowPostReviewFragment.kt
@@ -1,6 +1,9 @@
 package good.damn.tvlist.fragments.ui.main.tv_show_details
 
 import android.content.Context
+import android.text.Editable
+import android.text.InputFilter
+import android.text.TextWatcher
 import android.view.Gravity
 import android.view.View
 import android.widget.FrameLayout
@@ -20,16 +23,19 @@ import good.damn.tvlist.fragments.StackFragment
 import good.damn.tvlist.fragments.animation.FragmentAnimation
 import good.damn.tvlist.models.tv_show.TVShowReview
 import good.damn.tvlist.utils.ViewUtils
+import good.damn.tvlist.views.buttons.RoundButton
 import good.damn.tvlist.views.rate.RateView
 import good.damn.tvlist.views.text_fields.TextFieldRound
 import good.damn.tvlist.views.top_bars.TopBarView
 import good.damn.tvlist.views.top_bars.defaultTopBarStyle
 
 class TVShowPostReviewFragment
-: StackFragment() {
+: StackFragment(),
+TextWatcher {
 
     companion object {
         private const val TAG = "TVShowPostReviewFragmen"
+        private const val MAX_LENGTH_REVIEW = 700
         fun newInstance(
             tvShow: TVShowReview?,
             grade: Byte
@@ -41,6 +47,8 @@ class TVShowPostReviewFragment
 
     var review: TVShowReview? = null
     var grade: Byte = 0
+
+    private var mTextViewCounter: AppCompatTextView? = null
 
     override fun onCreateView(
         context: Context,
@@ -168,6 +176,16 @@ class TVShowPostReviewFragment
                 )
             }
 
+            addTextChangedListener(
+                this@TVShowPostReviewFragment
+            )
+
+            filters = arrayOf(
+                InputFilter.LengthFilter(
+                    MAX_LENGTH_REVIEW
+                )
+            )
+
             setHint(
                 R.string.hint_text_review
             )
@@ -177,8 +195,99 @@ class TVShowPostReviewFragment
             )
         }
 
+        mTextViewCounter = AppCompatTextView(
+            context
+        ).apply {
+
+            boundsLinear(
+                Gravity.TOP or Gravity.END,
+                right = measureUnit * 33.normalWidth(),
+                top = measureUnit * 10.normalWidth()
+            )
+
+            typeface = App.font(
+                R.font.open_sans_regular,
+                context
+            )
+
+            text = "0 / $MAX_LENGTH_REVIEW"
+
+            setTextSizePx(
+                measureUnit * 15.normalWidth()
+            )
+
+            setTextColor(
+                App.color(
+                    R.color.primaryColor
+                ).withAlpha(
+                    0.3f
+                )
+            )
+
+            layout.addView(
+                this
+            )
+        }
+
+        RoundButton(
+            context
+        ).apply {
+
+            boundsLinear(
+                Gravity.CENTER_HORIZONTAL,
+                width = (measureUnit * 347.normalWidth()).toInt(),
+                height = (measureUnit * 50.normalWidth()).toInt(),
+                top = measureUnit * 29.normalWidth()
+            )
+
+            text = getString(
+                R.string.post_review
+            )
+
+            typeface = App.font(
+                R.font.open_sans_bold,
+                context
+            )
+
+            cornerRadius = heightParams() * 0.25f
+
+            textSizeFactor = 0.34f
+            textColor = App.color(
+                R.color.btnText
+            )
+
+            setBackgroundColorId(
+                R.color.btnBack
+            )
+
+            layout.addView(
+                this
+            )
+
+        }
+
         return layout
     }
+
+    override fun beforeTextChanged(
+        s: CharSequence?,
+        start: Int,
+        count: Int,
+        after: Int
+    ) = Unit
+
+    override fun onTextChanged(
+        s: CharSequence?,
+        start: Int,
+        before: Int,
+        count: Int
+    ) {
+        mTextViewCounter?.text = "${s?.length} / $MAX_LENGTH_REVIEW"
+    }
+
+    override fun afterTextChanged(
+        s: Editable?
+    ) = Unit
 }
 
 private fun TVShowPostReviewFragment.onClickBtnBack(
---
+++ app/src/main/java/good/damn/tvlist/views/buttons/RoundButton.kt
@@ -0,0 +1,81 @@
+package good.damn.tvlist.views.buttons
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Typeface
+import androidx.annotation.ColorInt
+import good.damn.tvlist.views.round.RoundView
+
+class RoundButton(
+    context: Context
+): RoundView(
+    context
+) {
+
+    var text: String? = null
+
+    var textSizeFactor: Float = 0.2f
+
+    var typeface = Typeface.DEFAULT
+        set(v) {
+            field = v
+            mPaintText.typeface = v
+        }
+
+    @ColorInt
+    var textColor: Int = Color.RED
+        set(v) {
+            field = v
+            mPaintText.color = v
+        }
+
+    private val mPaintText = Paint()
+
+    private var mTextX = 0f
+    private var mTextY = 0f
+
+    override fun onLayout(
+        changed: Boolean,
+        left: Int,
+        top: Int,
+        right: Int,
+        bottom: Int
+    ) {
+        super.onLayout(
+            changed,
+            left,
+            top,
+            right,
+            bottom
+        )
+
+        mPaintText.textSize = height * textSizeFactor
+        mTextY = (height + mPaintText.textSize) * 0.45f
+
+        if (text == null) {
+            return
+        }
+
+        mTextX = (width - mPaintText.measureText(
+            text
+        )) * 0.5f
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        if (text == null) {
+            return
+        }
+
+        canvas.drawText(
+            text!!,
+            mTextX,
+            mTextY,
+            mPaintText
+        )
+    }
+
+}
