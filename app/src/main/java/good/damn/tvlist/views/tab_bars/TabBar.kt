5af5bb2391386caa3fa423a6e126a5f200ba00d0
GoodDamn <grigorydum@gmail.com>
2024-07-14 09:46:35 +0300

    TabBar: tabView click listeners

--- app/src/main/java/good/damn/tvlist/views/tab_bars/TabBar.kt
+++ app/src/main/java/good/damn/tvlist/views/tab_bars/TabBar.kt
@@ -1,6 +1,7 @@
 package good.damn.tvlist.views.tab_bars
 
 import android.content.Context
+import android.util.Log
 import android.view.View
 import android.widget.HorizontalScrollView
 import android.widget.LinearLayout
@@ -20,9 +21,15 @@ class TabBar(
     context: Context
 ): HorizontalScrollView(
     context
-) {
+), View.OnClickListener {
+
+    companion object {
+        private const val TAG = "TabBar"
+    }
 
     var interval = 0f
+
+    private var mSelectedView: View? = null
 
     private val mLayout = LinearLayout(
         context
@@ -68,10 +75,38 @@ class TabBar(
 
             setBackgroundColor(0)
 
+            setOnClickListener(
+                this@TabBar
+            )
+
             mLayout.addView(
                 this
             )
         }
+
+        mSelectedView = mLayout.getChildAt(0)
+        mSelectedView?.setBackgroundColorId(
+            R.color.lime
+        )
+    }
+
+    override fun onClick(
+        v: View?
+    ) {
+        Log.d(TAG, "onClick: $v")
+
+        mSelectedView?.apply {
+            setBackgroundColor(0)
+            invalidate()
+        }
+        v?.apply {
+            setBackgroundColorId(
+                R.color.lime
+            )
+            invalidate()
+        }
+
+        mSelectedView = v
     }
 
 }
